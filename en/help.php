af94771c1b47434758d3b98f439c892a32ae24a3
Ivo Schwizer <ivoschwizer@gmail.com>
2023-09-13 18:10:43 +0200

    hl

---
+++ en/help.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="de">
+<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# place: http://ogp.me/ns/place#">
+
+    <meta charset="UTF-8">
+    <title>Biltec AG | d</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="........">
+    <meta name="keywords" content="........">
+    <link rel="canonical" href="https://biltec.ch" />
+
+    <link rel="stylesheet" href="../main.css" />
+
+    <?php require_once 'head.php'; ?>
+</head>
+
+<body>
+    <section class="main">
+        <h1 class="big">help</h1>
+    </section>
+        
+    <?php require_once 'nav.php'; ?>
+    <?php require_once 'footer.php'; ?>
+    <?php require_once 'script.php'; ?>
+    <?php require_once 'googleanalytics.php'; ?>
+</body>
+</html>
