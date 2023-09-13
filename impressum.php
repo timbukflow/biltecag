17259af21fa1ba3ce5162a23c97baf14ac2f1a7e
Ivo Schwizer <ivoschwizer@gmail.com>
2023-09-13 17:45:32 +0200

    new

---
+++ impressum.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="de">
+<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# place: http://ogp.me/ns/place#">
+
+    <meta charset="UTF-8">
+    <title>Biltec AG  | Impressum</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="Biltec AG ">
+    <meta name="robots" content="noindex"> 
+
+    <!-- Favicon einfügen -->
+
+    <link rel="stylesheet" href="main.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap" rel="stylesheet">
+
+</head>
+
+<body>
+    <section class="datenschutz">
+        <h1>Impressum</h1>
+        <h3 class="">Verantwortlich für den Inhalt:</h3>
+        <p>
+            Biltec AG <br>
+            Unterlettenstrasse 10<br>
+            9443 Widnau<br><br>
+            Telefon +41 71 720 17 87<br>
+            info@biltec.ch<br>
+            <a href="https://biltec.ch">www.biltec.ch</a>
+        </p>
+
+        <h3 class="">Copyright</h3>
+        <p>
+            Das Copyright für sämtliche Inhalte dieser Website liegt bei Biltec AG, 9443 Widnau.
+        </p>
+
+        <h3 class="">Konzept und Technische Umsetzung</h3>
+        <p>Schwizer Design GmbH<br>
+        Gallusstrasse 43<br>
+        CH-9000 St. Gallen<br>
+        <a href="https://schwizerdesign.ch">schwizerdesign.ch</a></p>
+
+        <h3 class="">Haftungshinweis</h3>
+        <p>Trotz sorgfältiger inhaltlicher Kontrolle übernehmen wir keine Haftung für die Inhalte externer Links. Für den Inhalt der verlinkten Seiten sind ausschliesslich deren Betreiber verantwortlich.</p>
+
+        <h3 class="">Urheberrecht</h3>
+        <p>Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem schweizerischen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung ausserhalb der Grenzen des Urheberrechts bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet.</p>
+    </section>
+      
+    
+    <?php require_once 'nav.php'; ?>
+    <?php require_once 'footer.php'; ?>
+    <?php require_once 'script.php'; ?>
+    <?php require_once 'googleanalytics.php'; ?>
+</body>
+</html>
---
+++ nav.php
@@ -0,0 +1,18 @@
+<nav>
+    <a href="index" class="logoC">
+        <h1 class="logo">Biltec AG</h1>
+    </a>
+    <div class="navbar">
+        <div class="burger-icon">
+            <div class="bar"></div>
+            <div class="bar"></div>
+            <div class="bar"></div>
+        </div>
+        <ul class="nav-list" id="navList">
+            <li><a href="uebermich">Produkte</a></li>
+            <li><a href="uebermich">Unternehmen</a></li>
+            <li><a href="preise">Kontakt</a></li>
+            <li><a href="index">DE</a> | <a href="en">EN</a></li>
+        </ul>
+    </div>
+</nav>
